d1155e2bc04121dbed5e312c29be39e8e4128ac0
Ghalya Alkawari <s69galka@uni-bonn.de>
2025-09-09 18:04:45 +0000

    name

--- extra/server.py
+++ extra/server.py
@@ -25,3 +25,9 @@ def check_credentials(userID, password):
             if row['userID'] == userID and row['password'] == password:
                 return True
     return False
+
+
+
+if __name__ == '__main__':
+  this_python_file = os.path.basename(__file__)[:-3]
+  instance = uvicorn.run(f"{this_python_file}:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
